fef129554a6b18615152c7d4043f97403ed68d21
Stéphane Graber <stgraber@ubuntu.com>
2014-08-15 17:07:58 -0400

    Add map rendering
    Signed-off-by: Stéphane Graber <stgraber@ubuntu.com>

---
+++ map/map.js
@@ -0,0 +1,36 @@
+var map = L.map('map', {
+    minZoom: 2,
+    maxZoom: 10
+});
+
+//L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+L.tileLayer('http://{s}.www.toolserver.org/tiles/osm-no-labels/{z}/{x}/{y}.png', {
+    tileSize: 256,
+}).addTo(map);
+
+map.fitWorld();
+
+var routerIcon = L.icon({
+    iconUrl: 'router.png',
+    iconSize: [18, 18],
+});
+
+var tier0Icon = L.icon({
+    iconUrl: 'tier0.png',
+    iconSize: [26, 17],
+});
+
+var tier1Icon = L.icon({
+    iconUrl: 'tier1.png',
+    iconSize: [18, 18],
+});
+
+var tier2Icon = L.icon({
+    iconUrl: 'tier2.png',
+    iconSize: [18, 18],
+});
+
+var tier3Icon = L.icon({
+    iconUrl: 'tier3.png',
+    iconSize: [18, 18],
+});
